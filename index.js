05deeb1655560b230357b756bd6ba9eb693c53ba
Mac Rowe <m.rowe42@gmail.com>
2021-06-02 17:16:06 -0400

    commit tree fadfe37d571e2b865929043457a8aaa48b4a3803

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+function saturdayFun(string = "roller-skate") {
+    return `This Saturday, I want to ${string}!`;
+}
+
+function mondayWork(string = "go to the office") {
+    return `This Monday, I will ${string}.`
+}
+
+function wrapAdjective(fun = "*") {
+    return function(adjective = "special") {
+        return `You are ${fun}${adjective}${fun}!`
+    }
+
+}
+
+const Calculator = {
+
+    add: function(num1, num2) {
+        return num1 + num2;
+    },
+
+    subtract: function(num1, num2){
+        return num1 - num2;
+    },
+
+    multiply: function(num1, num2) {
+        return num1 * num2; 
+    },
+
+    divide: function(num1, num2) {
+        return num1 / num2;
+    }
+
+}
+
+function actionApplyer(number, array) {
+    for( let i = 0; i < array.length; i++ ) {
+        number = array[i](number);
+    }
+    return number;
+}
